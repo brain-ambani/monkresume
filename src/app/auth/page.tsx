6c5eb70002b87e85d234ef16cf772b8bc54684a8
brain ambani <brainambani1@gmail.com>
2024-09-11 07:04:09 +0300

    Create nextjs boilerplate for the project

--- src/app/auth/page.tsx
+++
@@ -1,8 +0,0 @@
-import { AuthScreen } from "@/features/auth/components/auth-screen";
-import React from "react";
-
-const AuthPage = () => {
-  return <AuthScreen />;
-};
-
-export default AuthPage;

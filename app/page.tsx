bc1af469f936b6b882b51747f0358c803f4b1962
Brain Ambani <brainambani1@gmail.com>
2024-09-13 08:04:56 +0300

    added the clerk authentication

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h2>Monk Resume</h2>
+      <p>An AI powered resume builder</p>
+    </main>
+  );
+}
